395911a950ae734af2eebdccfd021d6a1fa0de20
Chris Hammock <chris@crshmk.com>
2023-04-01 10:21:54 +0700

    add setup script

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SRC="/Users/pt/dev/scripts"
+SYMLINK="/usr/local/bin"
+
+ln -s $SRC/backup/backup-project/backup-project.sh $SYMLINK/backup-project
+ln -s $SRC/backup/backup-machine/backup-machine.sh $SYMLINK/backup-machine
+ln -s $SRC/backup/pull/pull.sh $SYMLINK/pull
+ln -s $SRC/init-project $SYMLINK/init-project
+ln -s $SRC/remote/index.js $SYMLINK/remote
+
+mv $SRC/backup/backup-project/mysql-cnf $SRC/backup/backup-project/mysql.cnf 
+mv $SRC/backup/backup-project/.env-example $SRC/backup/backup-project/.env 
+mv $SRC/backup/pull/.env-example $SRC/backup/pull/.env 
+mv $SRC/backup/backup-machine/backup-paths-example.txt $SRC/backup/backup-machine/backup-paths.txt 
+mv $SRC/backup/backup-machine/.env-example $SRC/backup/backup-machine/.env 
+mv $SRC/.env-example $SRC/.env 
